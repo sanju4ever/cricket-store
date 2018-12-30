b780cbbb3fa7366a401d364c1ae244c07e4b3c7f
Sanjeewa Jayasinghe <sanjeewa,jayasinghe25@gmail.com>
2018-12-30 15:25:07 +1100

    Products component initiated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+  `<div>
+  <h3>{{pageTitle}}</h3>
+  <p>Hello Cricket Store!</p>
+  <cric-products></cric-products>
+  </div>`
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent{
+  pageTitle: string = "Online Cricket Store";
 }
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'cric-products',
+    templateUrl: './products.component.html'
+})
+export class ProductsComponent {
+
+}
